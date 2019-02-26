78abb6ce18d17dd62b2ac9562c64991ea7a96867
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-02-26 13:42:49 -0600

    Created Chat Mutation

---
+++ src/NewChatMutation.js
@@ -0,0 +1,14 @@
+import gql from 'graphql-tag'
+
+const CREATE_CHAT_MUTATION = gql`
+  mutation CreateChatMutation($content: String!, $from: String!) {
+    createChat(content: $content, from: $from) {
+      id
+      createdAt
+      from
+      content
+    }
+  }
+`
+
+export default CREATE_CHAT_MUTATION
